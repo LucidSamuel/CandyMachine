2f57a1d09f0ddc9c0fe59bb0dad1366ee079ce7b
Samuel Akinosho <39565075+LucidSamuel@users.noreply.github.com>
2022-09-28 18:45:11 +0400

    fixing property variants

---
+++ candymachine/candymachine_assets/src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+
+const App: React.FC = () => {
+
+    return (
+        <div>
+            <p>Hi People of the World</p>
+        </div>
+
+    );
+}
+
+export default App
